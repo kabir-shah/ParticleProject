deaaec6455aef08a3011781da5a31d71a4c100bf
unknown <oleg.bychenkov@gmail.com>
2020-12-01 17:43:39 -0800

    Added pressure sensor

--- Sensors.java
+++ Sensors.java
@@ -12,12 +12,16 @@ public class Sensors extends Actor
     private double averageKinetic = 0;
     private double temperature = 0;
     private double totalVelocity = 0;
+    private double sumForce = 0;
+    private double pressure = 0;
     public Sensors(ParticleWorld world) {
-        getImage().clear();
+        getImage().scale(1000, 10);
     }
     public void act() {
         List<Particle> particles = getWorld().getObjects(Particle.class);
         sumKinetic = 0;
+        sumForce = 0;
+        pressure = 0;
         averageKinetic = 0;
         temperature = 0;
         for (int i = 0; i < particles.size(); i++){
@@ -26,7 +30,19 @@ public class Sensors extends Actor
         }
         averageKinetic = sumKinetic / particles.size();
         temperature = 2 * averageKinetic / (3 * 8.3145);
-        getWorld().showText("The temperature is: " + temperature + " K", 500, 500);
-
+        temperature *= 10000;
+        temperature = (double) Math.round(temperature);
+        temperature /= 10000;
+        getWorld().showText("The temperature is: " + temperature + " K", 850, 50);
+        
+        List<Particle> touchingParticles = getIntersectingObjects(Particle.class);
+        for (int j = 0; j < touchingParticles.size(); j++) {
+            sumForce += touchingParticles.get(j).getMass() * Math.sqrt(touchingParticles.get(j).getVelocityY() * touchingParticles.get(j).getVelocityY() + touchingParticles.get(j).getVelocityX() * touchingParticles.get(j).getVelocityX());
+        }
+        pressure = sumForce / 3000;
+        pressure *= 100;
+        pressure = (double) Math.round(pressure);
+        pressure /= 100;
+        getWorld().showText("The pressure is: " + pressure + " Pa", 830, 70);
     }
 }
