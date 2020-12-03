6df902107827627583ad702f211295c0c5a78715
OlgBch <oleby0406@gmail.com>
2020-12-02 18:37:44 -0800

    Fixed sensor numbers

--- Sensors.java
+++ Sensors.java
@@ -24,9 +24,16 @@ public class Sensors extends Actor
             totalVelocity = Math.sqrt(particles.get(i).getVelocityY() * particles.get(i).getVelocityY() + particles.get(i).getVelocityX() * particles.get(i).getVelocityX());
             sumKinetic += (particles.get(i).getMass()) * totalVelocity * totalVelocity;
         }
-        temperature = sumKinetic / (1.38064852 * 1e-23 * (3* particles.size() - 3));
+        temperature = sumKinetic / (1.38064852 * 1e-23 * 6.022*1e23 * (3* particles.size() - 3));
         pressure = particles.size() * 8.20573 * 101325 * temperature / 30000;
+        temperature *= 10000;
+        temperature = (double) Math.round(temperature);
+        temperature /= 10000;
+        pressure *= 10000;
+        pressure = (double) Math.round(pressure);
+        pressure /= 10000;
+        
         getWorld().showText("The pressure is: " + pressure + " Pa", 825, 75);  
-        getWorld().showText("The temperature is: " + temperature + " K", 650, 50);  
+        getWorld().showText("The temperature is: " + temperature + " K", 820, 50);  
     }
 }
