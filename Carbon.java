190f0c6e836554e732080b4107a819ded19628a1
kklike32 <44389276+kklike32@users.noreply.github.com>
2020-12-02 20:03:05 -0800

    comments

--- Carbon.java
+++ Carbon.java
@@ -2,9 +2,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class Carbon here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
+ * This Carbon class describes the size and mass of the particle so other classes and methods can use it. It also sets the image of the particle. 
+ *
  */
 public class Carbon extends Particle
 {
