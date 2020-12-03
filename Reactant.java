07ea16d7cc0dcdd0094f46b6ffce61cd473885b0
kklike32 <44389276+kklike32@users.noreply.github.com>
2020-12-03 08:47:23 -0800

    trying toString

--- Reactant.java
+++ Reactant.java
@@ -6,7 +6,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Reactant extends Actor
 {
     public int coefficient; // The stoichiometric coefficient of the product.
-    public Class particle; // The particle class of a reactant.
+    public String particle; // The particle class of a reactant.
 
     /**
      * Create a new reactant.
@@ -14,5 +14,9 @@ public class Reactant extends Actor
     public Reactant(int c, Class p) {
         coefficient = c;
         particle = p;
+    }
+    
+    public String toString() {
+        return "particle";
     }
 }
