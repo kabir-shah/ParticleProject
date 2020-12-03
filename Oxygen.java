ee3475c6e1f37edccebb36a97ff10d89c69d74ec
kklike32 <44389276+kklike32@users.noreply.github.com>
2020-12-02 20:06:11 -0800

    comments

--- Oxygen.java
+++ Oxygen.java
@@ -3,8 +3,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Oxygen here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * This Oxygen class describes the size and mass of the particle so other classes and methods can use it. It also sets the image of the particle. 
  */
 public class Oxygen extends Particle
 {
